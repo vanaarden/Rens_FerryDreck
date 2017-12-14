2228a25c3d609f29256667ad85864926dcaefcf1
Margo van de Pas <margo@MacBook-Pro-van-Margo.local>
2017-12-14 09:56:26 +0100

    kleine aanpassing

--- index.php
+++ index.php
@@ -1,31 +1,32 @@
 <!DOCTYPE html>
 <html lang="nl">
+
 <head>
     <meta charset="utf-8">
     <title>Vul hier de naam van je dier in</title>
 </head>
+
 <body>
-<h1>Vul de naam van een dier in</h1>
-<input type="text" name="text" alt="text" id="text">
-<div id="element"></div>
-<script>
+    <h2>Vul de naam van een dier in</h2>
+    <input type="text" name="text" alt="text" id="text">
+    <div id="element"></div>
+    <script>
+        document.addEventListener("DOMContentLoaded", () => {
 
-    document.addEventListener("DOMContentLoaded", () => {
+                    const text = document.getElementById('text');
+                    const element = document.getElementById('element');
+                    text.addEventListener("keyup", () => {
+                            const xhttp = new XMLHttpRequest();
+                            xhttp.addEventListener('load', function() {
+                                    if (this.readyState === 4 && this.status === 200) {
+                                        element.innerHTML = this.responseText;
 
-        const text = document.getElementById('text');
-        const element = document.getElementById('element');
-        text.addEventListener("keyup", () => {
-            const xhttp = new XMLHttpRequest();
-            xhttp.addEventListener('load', function () {
-                if (this.readyState === 4 && this.status === 200) {
-                    element.innerHTML = this.responseText;
+                                    }
+                                }; xhttp.open("GET", "ajaxtest.php?dier=" + text.value, true); xhttp.send();
+                            });
+                    });
 
-                }
-            };
-            xhttp.open("GET", "ajaxtest.php?dier=" + text.value, true);
-            xhttp.send();
-        });
-    });
-</script>
+    </script>
 </body>
+
 </html>
